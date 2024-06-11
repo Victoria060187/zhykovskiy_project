46d034c5d1ef2077fb3b35fa120de860698f3392
Vitaliy Laputko <vitaliy.laputko@playtika.com>
2024-06-11 12:57:47 +0300

    created component header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,28 @@
+// import "./Header.css"
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header_row">
+          <div className="header_logo">
+            <div>logo</div>
+            <h3>Zhykovskiy & Co</h3>
+          </div>
+          <nav className="header_nav">
+            <div className="header_nav-main">
+              <ul>
+                <li className="heder_nav a">Головна</li>
+                <li className="heder_nav a">Напрямки діяльності</li>
+                <li className="heder_nav a">Про нас</li>
+                <li className="heder_nav a">Запис</li>
+                <li className="heder_nav a">Контакти</li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
+export default Header;
