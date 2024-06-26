2f672f7d2b1d4b1db34984636b906514c722fdb5
Victoria Laputko <vikalaputko87@gmail.com>
2024-06-26 12:34:27 +0300

    added a content AboutUs page

---
+++ src/components/aboutUs/AboutUs.jsx
@@ -0,0 +1,20 @@
+import './AboutUs.css';
+
+function AboutUs() {
+  return (
+    <section className="about-us">
+      <div className="about-container">
+        <div className="about-content">
+          <div className="about-title">About Us</div>
+          <div className="about-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime inventore ratione
+            dolorum vitae ea explicabo quisquam dolorem ullam tempora tempore velit repellendus
+            soluta voluptates a, qui, eos aliquid perspiciatis facere.
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default AboutUs;
