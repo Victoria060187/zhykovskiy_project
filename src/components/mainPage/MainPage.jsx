39f094ace3f77cf0f995dda633011a17aa36e72c
Victoria Laputko <vikalaputko87@gmail.com>
2024-06-26 13:38:09 +0300

    wrote the last name correctly in the MainPage

--- src/components/mainPage/MainPage.jsx
+++ src/components/mainPage/MainPage.jsx
@@ -6,7 +6,7 @@ function MainPage() {
     <section className="main-page" style={{ backgroundImage: `url(${MainImg})` }}>
       <div className="main-container">
         <div className="main-content">
-          <div className="main-title">Zhukovskiy & Co</div>
+          <div className="main-title">Zhukovskyi & Co</div>
           <div className="main-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime inventore ratione
             dolorum vitae ea explicabo quisquam dolorem ullam tempora tempore velit repellendus
